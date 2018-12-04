b227b672080c8876b8156dbb9a16437b45adfb96
Angelina Li <ali6@wellesley.edu>
2018-12-04 10:11:20 -0500

    added smaller 0.5 samples

--- scripts/create_samples.R
+++ scripts/create_samples.R
@@ -5,6 +5,25 @@
 
 setwd("~/Desktop/Code/in_progress/blue-bikes/")
 
+set.seed(1)
 df <- readRDS("data/intermediate/hubway.rds")
+nrow(df)
 
-# rest of your code here
+# took 50% sample of entire  dataset first for manageability
+df <- df[sample(nrow(df), nrow(df) * 0.5),]
+nrow(df)
+df["segment"] <- runif(nrow(df), 0, 3)
+
+train <- df[df$segment <= 1, !(names(df) %in% c("segment"))]
+nrow(train)
+colnames(train)
+
+test <- df[(df$segment > 1) & (df$segment <= 2), !(names(df) %in% c("segment"))]
+nrow(test)
+
+validate <- df[(df$segment > 2) & (df$segment <= 3), !(names(df) %in% c("segment"))]
+nrow(validate)
+
+saveRDS(train, "data/intermediate/train.rds")
+saveRDS(test, "data/intermediate/test.rds")
+saveRDS(validate, "data/intermediate/validate.rds")
