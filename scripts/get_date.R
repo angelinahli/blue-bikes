b5dfaa0127642ef74f8c9ee01a882025724f5447
Angelina Li <ali6@wellesley.edu>
2018-12-06 11:36:01 -0500

    added get_date starter file

--- scripts/get_date.R
+++ scripts/get_date.R
@@ -1,7 +1,7 @@
-# filename: create_datasets.R
+# filename: get_date.R
 # author: Angelina Li
-# date: 12/04/2018
-# desc: create datasets to use.
+# date: 12/06/2018
+# desc: weekday riderships
 
 required.pkgs <- c("lubridate")
 new.pkgs <- required.pkgs[!(required.pkgs %in% installed.packages()[,"Package"])]
@@ -12,17 +12,7 @@ library(lubridate)
 # change to the folder holding your code
 setwd("~/Desktop/Code/in_progress/blue-bikes/")
 
-## uncomment whichever dataset you need
 df.test <- readRDS("data/intermediate/test.rds")
 df.test$weekday <- wday(df.test$starttime)
-
-df.test[1:2,]
-
-colnames(df.test)
-wday(df.test$starttime[1])
-df$start.station.name[1]
-df$start.station.id[1]
-
-sort(unique(df$start.station.id))[1:5]
-nrow(df.test[df.test$start.station.id == 163])
-
+df.test$weekday[1:5]
+unique(df.test$weekday) # ranges from 1 - 7
